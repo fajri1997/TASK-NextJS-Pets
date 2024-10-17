a0b3d523fb5d1034fe1afc67225562e708476ca3
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-10-17 06:27:13 +0100

    done

--- src/app/Components/PetsList.js
+++ src/app/Components/PetsList.js
@@ -1,24 +1,42 @@
+"use client";
 import pets from "../data/pets";
 import PetItem from "./PetItem";
-
+import { useState } from "react";
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("");
 
+  const filteredPets = pets.filter(
+    (pet) =>
+      pet.name.toLowerCase().includes(query.toLowerCase()) &&
+      (type === "" || pet.type === type)
+  );
+
+  const petList = filteredPets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+
+  function handleSearch(event) {
+    setQuery(event.target.value);
+    console.log(event.target.value);
+  }
+  function handlePetSelector(event) {
+    setType(event.target.value);
+  }
   return (
     <>
       <div className="mx-auto">
         <div className="flex justify-start space-x-2 w-full font-primary">
           <div className="flex flex-col items-start space-y-1 flex-grow">
             <input
-              type="search"
               placeholder="search"
               className="text-gray-900 form-input border border-gray-300 w-full rounded-sm focus:border-palette-light focus:ring-palette-light"
+              onChange={handleSearch}
             />
           </div>
           <div className="flex flex-col items-start space-y-1 flex-grow-0">
             <select
               defaultValue={""}
               className="form-select border border-gray-300 rounded-sm w-full text-gray-900 focus:border-palette-light focus:ring-palette-light"
+              onChange={handlePetSelector}
             >
               <option value="">All</option>
               <option value="Cat">Cat</option>
@@ -28,7 +46,9 @@ function PetsList() {
           </div>
         </div>
       </div>
-      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">{petList}</div>
+      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        {petList}
+      </div>
     </>
   );
 }
